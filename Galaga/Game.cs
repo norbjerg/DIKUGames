517627772733d3647aca7d29cd21e96dbdb9837f
norbjerg <rzd350@alumni.ku.dk>
2023-02-26 18:37:20 +0100

    6.2

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -4,17 +4,25 @@ using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 using DIKUArcade;
 using DIKUArcade.GUI;
+using DIKUArcade.Events;
+using DIKUArcade.Input;
+using System.Collections.Generic;
 
 namespace Galaga
 {
-    public class Game : DIKUGame
+    public class Game : DIKUGame, IGameEventProcessor
     {
         private Player player;
+        private GameEventBus eventBus;
 
         public Game(WindowArgs windowArgs) : base(windowArgs) {
             player = new Player(
                 new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
                 new Image(Path.Combine("Assets", "Images", "Player.png")));
+            eventBus = new GameEventBus();
+            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.InputEvent });
+            window.SetKeyEventHandler(KeyHandler);
+            eventBus.Subscribe(GameEventType.InputEvent, this);
         }
 
         public override void Render()
@@ -26,6 +34,41 @@ namespace Galaga
         public override void Update()
         {
             window.PollEvents();
+            eventBus.ProcessEventsSequentially();
+            player.Move();
+        }
+
+        private void KeyPress(KeyboardKey key) {
+            // TODO: Close window if escape is pressed
+            // TODO: switch on key string and set the player's move direction
+            switch (key) {
+                case KeyboardKey.Escape:
+                    eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.WindowEvent });
+                    break;
+                case KeyboardKey.Left:
+                    player.SetMoveLeft(true);
+                    break;
+                case KeyboardKey.Right:
+                    player.SetMoveRight(true);
+                    break;
+            }
+        }
+        private void KeyRelease(KeyboardKey key) {
+            // TODO: switch on key string and disable the player's move direction
+            switch (key) {
+                case KeyboardKey.Left:
+                    player.SetMoveLeft(false);
+                    break;
+                case KeyboardKey.Right:
+                    player.SetMoveRight(false);
+                    break;
+            }
+        }
+        private void KeyHandler(KeyboardAction action, KeyboardKey key) {
+            // TODO: Switch on KeyBoardAction and call proper method
+        }
+        public void ProcessEvent(GameEvent gameEvent) {
+            // Leave this empty for now
         }
     }
 }
--- Galaga/Player.cs
+++ Galaga/Player.cs
@@ -4,14 +4,52 @@ namespace Galaga {
 public class Player {
         private Entity entity;
         private DynamicShape shape;
+        private float moveLeft;
+        private float moveRight;
+        private const float MOVEMENT_SPEED = 0.01f;
 
         public Player(DynamicShape shape, IBaseImage image) {
             entity = new Entity(shape, image);
             this.shape = shape;
+            moveLeft = 0.0f;
+            moveRight = 0.0f;
         }
 
         public void Render() {
             entity.RenderEntity();
+        }
+
+
+        public void Move() {
+            // TODO: Finish this, so it uses the right max and min for both X and Y
+            if (shape.Position.X >= 0 && shape.Position.X <= 100) {
+                shape.Move();
+            }
+            // TODO: move the shape and guard against the window borders
+        }
+        public void SetMoveLeft(bool val) {
+            if (val) {
+                moveLeft = -MOVEMENT_SPEED;
+            }
+            else {
+                moveLeft = 0;
+            }
+            UpdateDirection();
+            // TODO:set moveLeft appropriately and call UpdateDirection()
+        }
+        public void SetMoveRight(bool val) {
+            if (val) {
+                moveLeft = MOVEMENT_SPEED;
+            }
+            else {
+                moveLeft = 0;
+            }
+            UpdateDirection();
+            // TODO:set moveRight appropriately and call UpdateDirection()
+        }
+
+        private void UpdateDirection() {
+            shape.Direction.X = moveLeft + moveRight;
         }
     }
 }
