ff92f2e74aef79e925e15677edc7638ee5523fd5
norbjerg <rzd350@alumni.ku.dk>
2023-03-12 18:07:04 +0100

    rethough enemies

--- Galaga/Enemy.cs
+++ Galaga/Enemy.cs
@@ -2,6 +2,36 @@ using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 namespace Galaga;
 public class Enemy : Entity {
-    public Enemy(DynamicShape shape, IBaseImage image)
-    : base(shape, image) {}
+
+    private int hitpoints;
+    private float speed;
+    private IBaseImage enemyStrideRed;
+
+    public Enemy(DynamicShape shape,
+        IBaseImage image,
+        IBaseImage enemyStrideRed)
+    : base(shape, image) {
+        hitpoints = 4;
+        this.enemyStrideRed = enemyStrideRed;
+        speed = 0.01f;
+    }
+
+    /// <summary>
+    /// Method for enemy getting shot, returns true, if enemy dies.
+    /// Checks if the enemy should get enraged and turn red.
+    /// </summary>
+    public bool getShot() {
+        hitpoints -= 1;
+        if (hitpoints < 0) {
+            return true;
+        }
+        // Becomes enraged
+        else if (hitpoints == 2) {
+            hitpoints -= 1;
+            this.Image = this.enemyStrideRed;
+            speed *= 2;
+            return false;
+        }
+        return false;
+    }
 }
